835dad199b136212faedb0c983a21071af79741c
Divya Thakkar <divyathakkar1508@gmail.com>
2025-10-13 14:32:47 +0530

    Delete ex3 directory

--- ex3/tailwind.config.js
+++
@@ -1,9 +0,0 @@
-function changeColor(){
-    document.getElementById("colorid").style.color=rendomColor();
-}
-function rendomColor(){
-    return '#' + Math.floor(Math.random() * 16777215).toString(16);
-}
-
-
-changeColor();
